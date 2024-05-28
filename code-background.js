a9972aab71cbd68433fcab836276d50de989e611
PERPELEX <hannanamir18@gmail.com>
2024-05-28 16:22:33 +0500

    Bad Contrast
    Made contrast changes

---
+++ code-background.js
@@ -0,0 +1,27 @@
+function getRandomSize() {
+    return Math.floor(Math.random() * 20) + 10;
+}
+
+function getRandomPosition() {
+    return Math.random() * 100;
+}
+
+function getRandomDelay() {
+    return Math.random() * 5;
+}
+
+function getRandomSpeed() {
+    return Math.random() * 10 + 5; 
+}
+
+
+for (let i = 0; i < 13; i++) {
+    const bubble = document.createElement("div");
+    const container = document.querySelector(".home");
+    bubble.classList.add("bubble");
+    bubble.style.width = bubble.style.height = `${getRandomSize()}px`;
+    bubble.style.left = `${getRandomPosition()}%`;
+    bubble.style.animationDuration = `${getRandomSpeed()}s`;
+    bubble.style.animationDelay = `${getRandomDelay()+0}s`; 
+    container.appendChild(bubble);
+}
